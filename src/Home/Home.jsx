9c3e41fe5448d8b1fefa84295cd8f26bb6e95838
Josemario <97921175+Josemario17@users.noreply.github.com>
2024-06-23 01:17:30 +0100

    NavBar added

---
+++ src/Home/Home.jsx
@@ -0,0 +1,12 @@
+
+import { Link } from 'react-router-dom'
+import logo from '../assets/img/logo3.png'
+import NavBarLeft from '../Components/MainComponentes/NavBarLeft'
+
+export default function Home() {
+    return (
+        <div className="w-screen h-screen">
+            <NavBarLeft></NavBarLeft>
+        </div>
+    )
+}
--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
 import Loader from "./Login/Loader";
+import Home from "./Home/Home";
 
 export default function AppRoutes(){
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Loader/>}></Route>
+                <Route path="/Home" element={<Home/>}></Route>
             </Routes>
         </BrowserRouter>
     )
